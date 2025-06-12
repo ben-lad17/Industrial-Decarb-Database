f1ca0ecfeaaecd4bd66b50f0097e1ca626712c01
ben-lad17 <bladabaum@ucsb.edu>
2025-06-12 15:01:20 -0700

    Remove duplicates

--- test folder/Functions/convert_to_numeric.R
+++
@@ -1,5 +0,0 @@
-# convert variables from string to numeric
-convert_to_numeric <- function(data, columns) {
-  data[columns] <- lapply(data[columns], function(col) as.numeric(as.character(col)))
-  return(data)
-}
